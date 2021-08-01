f25ca7401a506df6c6c2e5bbf78c7d63e0e20c84
Rozana Alam <61743319+Rozana-github@users.noreply.github.com>
2021-08-02 00:28:36 +0200

    commit
    Basic C++ code

---
+++ ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << (int)'a' << endl; // to check the A
+	cout << (int)'A' << endl;
+
+	cout << char(65) << endl;
+	cout << char(97) << endl;
+
+	system("pause>0");
+}
+
+// this program is sho s wing how the ASCII value and  character related //
---
+++ cijercipher.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char c1, c2, c3, c4, c5, c6;
+	cout << "enter 6 character :";
+	cin >> c1 >> c2 >> c3 >> c4 >> c5 >> c6;
+	cout << "ASCII message value :" << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << "" << int(c6) << "";
+	system("pause>0");
+}
